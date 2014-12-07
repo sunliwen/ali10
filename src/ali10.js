1b6b0abca61ddb76466703b00ce1952297b2e8f7
Liwen Sun <sunliwen@gmail.com>
2014-12-08 05:00:36 +0800

    Random result after pick up a choice

--- src/ali10.js
+++ src/ali10.js
@@ -20,6 +20,17 @@ $(function () {
         $('section.welcome').hide();
         $('section.idea').hide();
         $('section.answer').show();
+
+        var result = results[choice.toLowerCase()];
+        var length = Object.keys(result).length;
+        var i = getRandomInt(length);
+        var result = result[i.toString()];
+        $('.answer_text').text(result.long);
+        document.title = result.short;
+    });
+
+    $('button.choice').on('click', function(){
+        choice = $(this).data('choice');
     });
 
     $('.share_btn').on('click', function(){
@@ -27,5 +38,11 @@ $(function () {
         console.log(results);
     });
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * (max + 1));
+    }
+
     var results = {};
+    var choice = null;
+    var result = null;
 });
